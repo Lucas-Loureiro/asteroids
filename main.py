cef0ec5c693ef798dc2ed4977f801692983ab7db
Lucas-Loureiro <loureiro4778@gmail.com>
2025-02-20 02:53:20 -0300

    Finalizing the asteroid game

--- main.py
+++ main.py
@@ -1,15 +1,57 @@
+import sys
 import pygame
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers =  updatable
+    asteroid_field = AsteroidField()
+
+
+    Player.containers = (updatable, drawable)
+    player = Player(x, y)
+
+
+
     while True:
-        pygame.Surface.fill(screen, (0,0,0))                                
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt) 
+        screen.fill("black")
+
+        for j in asteroids:
+            if j.collision(player):
+                print("Game Over!")
+                sys.exit()
+            for k in shots:
+                if k.collision(j):
+                    j.split()
+                    k.kill()
+
+        for i in drawable:
+          i.draw(screen)
+
+
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
 
 
 if __name__ == "__main__":
